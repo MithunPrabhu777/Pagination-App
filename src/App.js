195b34db2bd6fdb10dadc837309edfb4d156117f
MithunPrabhu777 <prabhuofficial7@gmail.com>
2021-08-18 22:58:35 +0530

    fetching data from api added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import "./App.css";
-import React, { useState } from "react";
-import JsonData from "./MOCK_DATA.json";
+import React, { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
+import axios from 'axios';
 
 function App() {
-  const [users, setUsers] = useState(JsonData.slice(0, 50));
+  const [users, setUsers] = useState([]);
+  const [data,setData] = useState({});
   const [pageNumber, setPageNumber] = useState(0);
 
-  const usersPerPage = 10;
-  const pagesVisited = pageNumber * usersPerPage;
+
+  useEffect(async () => {
+    const response1 = await response1Data();
+    setUsers(response1);
+  },[]);
+
+  useEffect(async () => {
+    const response = await responseData();
+    setData(response);
+  },[]);
+
+  const responseData = async() => {
+    const response =  await axios.get('https://reqres.in/api/users?page=2');
+   console.log(response?.data);
+    return response?.data;
+  }
+
+const response1Data = async() => {
+  const response =  await axios.get('https://reqres.in/api/users?page=2');
+ console.log(response?.data?.data.map((user) => user));
+  return response?.data?.data.map((user) => user);
+}
+
+
+  const pagesVisited = pageNumber * data?.per_page;
 
   const displayUsers = users
-    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .slice(pagesVisited, pagesVisited + data?.per_page)
     .map((user) => {
       return (
         <div className="user">
-          <h3>{user.firstName}</h3>
-          <h3>{user.lastName}</h3>
+          <img src={user?.avatar} style={{borderRadius:"50%"}}></img>
+          <h3>{user.first_name}</h3>
+          <h3>{user.last_name}</h3>
           <h3>{user.email}</h3>
         </div>
       );
     });
 
-  const pageCount = Math.ceil(users.length / usersPerPage);
+  const pageCount = Math.ceil(data?.total / data?.per_page);
 
   const changePage = ({ selected }) => {
     setPageNumber(selected);
